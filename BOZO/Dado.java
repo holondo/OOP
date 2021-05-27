65de6b19a7c338d37b7ac80cb70a4fcf58295bd8
holondo <holondo@outlook.com>
2021-05-27 18:44:53 -0300

    Dice simulator for BOZO game.

--- BOZO/Dado.java
+++ BOZO/Dado.java
@@ -2,15 +2,17 @@
  * This class is designed to simulate a rolling die
  * Developed for OOP class
  * @author Holondo
- *
  */
 public class Dado {
 
-    private int facesAmnt;
-    private int facingUp;
-    private Random die;
+    private int facesAmnt; //Number of sides
+    private int facingUp; //Which side is facing up
+    private Random die; //Random numbers object
 
-
+    /**
+	 * Inicializes the die with a random seed and 
+     * the deafault number of sides (6).
+	 */
     public Dado()
     {
         facesAmnt = 6;
@@ -18,7 +20,12 @@ public class Dado {
         for (Long i = 0l; i < 1000000; i++) {}
         die = new Random();
     }
-    
+
+    /**
+	 * Inicializes the die with a random seed and 
+     * a specified number od sides.
+	 * @param n Number of sides.
+	 */
     public Dado(int n)
     {
         facesAmnt = n;
@@ -26,19 +33,30 @@ public class Dado {
         die = new Random();
     }
 
+    /**
+	 * Simulates a die's roll.
+	 * @return (int) The side that is facing up.
+	 */
     public int rolar()
     {
-        
         facingUp = die.getIntRand(facesAmnt) + 1;
         
         return facingUp;
     }
 
+    /**
+	 * Returns the top side of the die.
+	 * @return (int) The side that is facing up.
+	 */
     public int getLado()
     {
         return facingUp;
     }
 
+    /**
+	 * Returns the formatted ASCII Art of the top of the die.
+	 * @return (String) Formatted ASCII Art.
+	 */
     @Override
     public String toString()
     {
